c23cb90b2b483cef82ac3a34456e83eca9d4beeb
Tiago Shebar <47699554@est.ort.edu.ar>
2024-05-19 19:02:46 -0300

    fALTA

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,6 @@ import Educacion from "./views/Educacion";
 import Certificaciones from "./views/Certificaciones";
 import Contacto from "./views/Contacto";
 import FooterComponent from "./components/FooterComponent";
-import InfoAcercaDeMi from "./infoAcercaDeMi.txt";
-import CardsExperiencia from "./cardsExperiencia.txt";
-import CardsEducacion from "./cardsEducacion.txt";
 
 
 const listaRutas = {
@@ -68,23 +65,34 @@ function App() {
       setImages(importedImages);
     });
   }, []);
+  const navBar = cargarNavBar();
+  const i = cargarPropsInicio();
+  const a = cargarPropsAcercaDeMi();
+  const cono = cargarPropsConocimientos();
+  const ex = cargarPropsExperiencia();
+  const ed = cargarPropsEducacion();
+  const ce = cargarPropsCertificaciones();
+  const cont = cargarPropsContacto();
+  const footerComponent = cargarFooterComponent();
   return (
     <>
-      <header><NavBar props={cargarNavBar()}/></header>
+      <header><NavBar listaNavlink={navBar}/></header>
       <main>
-        <Inicio props={cargarPropsInicio()}/>
-        <AcercaDeMi props={cargarPropsAcercaDeMi()}/>
-        <Conocimientos props={cargarPropsConocimientos()}/>
-        <Experiencia props={cargarPropsExperiencia()}/>
-        <Educacion props={cargarPropsEducacion()}/>
-        <Certificaciones props={cargarPropsCertificaciones()}/>
-        <Contacto props={cargarPropsContacto()}/>
+        <Inicio image={i.image} textoPrimero={i.textoPrimero} titulo={i.titulo} textoSegundo={i.textoSegundo} listaIconLink={i.listaIconLink} linkButton={i.linkButton} textoButton={i.textoButton}/>
+        <AcercaDeMi titulo={a.titulo} texto1={a.texto1} textoRowIzquierda1={a.textoRowIzquierda1} textoRowDerecha1={a.textoRowDerecha1} textoRowIzquierda2={a.textoRowIzquierda2} textoRowDerecha2={a.textoRowDerecha2} texto2={a.texto2} image={a.image}/>
+        <Conocimientos title={cono.title} listaImageText={cono.listaImageText}/>
+        <Experiencia titulo={ex.titulo} listaCards={ex.listaCards}/>
+        <Educacion titulo={ed.title} listaCards={ed.listaCards}/>
+        <Certificaciones imageModal={ce.imageModal}/>
+        <Contacto title={cont.title} h5title={cont.h5title} texto1={cont.texto1} texto2={cont.texto2} textoRowIzquierda={cont.textoRowIzquierda} textoRowDerecha={cont.textoRowDerecha}/>
       </main>
-      <footer><FooterComponent props={cargarFooterComponent()}/></footer>
+      <footer><FooterComponent props={footerComponent}/></footer>
     </>
     
   );
 }
+
+
 
 export default App;
 
@@ -118,18 +126,16 @@ const cargarPropsInicio = () => {
   };
 };
 
+
 const cargarPropsAcercaDeMi = () => {
-  console.log(InfoAcercaDeMi);
-  const a = require('./infoAcercaDeMi.txt');
-  console.log(a);
   return {
     titulo: "Hola! Me llamo Ezequiel Binker.",
-    texto1: paragraphs[0],
+    texto1: "Soy Licenciado en Sistemas recibido en la Universidad de Palermo. Además, hace más de 9 años que trabajo en educación IT, transmitiendo los conceptos de programación a cientos de jóvenes de nivel secundario y terciario.",
     textoRowIzquierda1: "Edad:",
     textoRowDerecha1: "29 años",
     textoRowIzquierda2: "Email:",
     textoRowDerecha2: "ezebinker@gmail.com",
-    texto2: paragraphs[1],
+    texto2: "Mis conocimientos en educación me permiten comunicar mis pensamientos e intenciones de una manera clara y eficiente. Además, cuento con muchas ganas de participar en proyectos que exploren tecnologías emergentes e innovadoras.",
     image: listaRutas.binker
   };
   
@@ -148,7 +154,6 @@ const cargarPropsConocimientos = () => {
 };
 
 const cargarPropsExperiencia = () => {
-  var paragraphs;
   return {
     titulo: "Experiencia Laboral",
     listaCards: [
@@ -156,49 +161,69 @@ const cargarPropsExperiencia = () => {
         imageCard: listaRutas.server,
         titleCard: "Desarrollador Backend",
         dondeYCuandoCard: "Wadiah Capital. (2021-2023)",
-        textoCard: paragraphs[0]
+        textoCard: `Trabajé durante 2 años en el rediseño funcional del sistema informático de Wadiah Capital. Comencé a cargo de un módulo que realiza una integración con servicios crypto, para luego quedar a cargo del core bancario. El stack de backend utilizado está comprendido por Nest.js (Express) conectado a una base de datos SQL Server. Además, otras herramientas utilizadas durante mi trabajo día a día son : Redis, CircleCI, Docker, Sentry y Azure. Dentro de mis tareas de I+D realicé algunos proyectos crypto, en los cuales puse en práctica conocimientos de Solidity, Pinata Cloud y OpenSea
+        <br>
+        <br>
+        El stack de backend utilizado está comprendido por Nest.js (Express) conectado a una base de datos SQL Server. Además, otras herramientas utilizadas durante mi trabajo día a día son : Redis, CircleCI, Docker, Sentry y Azure.
+        <br>
+        <br>
+        Dentro de mis tareas de I+D realicé algunos proyectos crypto, en los cuales puse en práctica conocimientos de Solidity, Pinata Cloud y OpenSea`
       },
       {
         imageCard: listaRutas.full_stack,
         titleCard: "Desarrollador Full Stack",
         dondeYCuandoCard: "Freelance. (2019-actual)",
-        textoCard: paragraphs[1]
+        textoCard: `Durante este tiempo, he adquirido experiencia en proyectos personales, tanto en el desarrollo Frontend utilizando tecnologías como HTML, CSS, y React.js, como en el desarrollo Backend empleando TypeScript, Node.js y Nest.js. Además, he incursionado en el desarrollo móvil utilizando React Native.
+        <br>
+        <br>
+        Mis habilidades destacadas se centran en el uso de NestJS y TypeScript, áreas en las que he acumulado experiencia significativa a lo largo de mi trayectoria profesional.`
       },
       {
         imageCard: listaRutas.web_programming_chico,
         titleCard: "Profesor de Informática",
         dondeYCuandoCard: "Escuela ORT. (2016-actual)",
-        textoCard: paragraphs[2]
+        textoCard: `Mi experiencia docente en ORT Argentina ha sido variada y enriquecedora, desempeñando múltiples roles que han moldeado mi trayectoria en educación informática. En "Taller de Programación", enseño fundamentos en desarrollo informático: desde C# hasta HTML, CSS, Bootstrap, MVC en .NET Core, Dapper ORM y JavaScript para bases de datos y vistas dinámicas.
+        <br>
+        <br>
+        En "Proyectos de Producción", guío a alumnos en la integración de sus conocimientos en un proyecto propio, destacando buenas prácticas de programación, trabajo en equipo, gestión de recursos y metodologías ágiles.
+        <br>
+        <br>
+        Como profesor de Diseño Web, priorizo el enfoque centrado en el usuario, UX, UI y Design Thinking con HTML, CSS para productos digitales significativos.
+        <br>
+        <br>
+        En "Bases de Datos" y "Tecnologías de la Información", complemento el estudio técnico con bases de hardware para soluciones informáticas acordes a las necesidades del cliente.`
       },
       {
         imageCard: listaRutas.algorithm,
         titleCard: "Profesor de Taller de Programación",
         dondeYCuandoCard: "Instituto ORT. (2020-2021)",
-        textoCard: paragraphs[3]
+        textoCard: `Impartí conocimientos en el "Taller de Herramientas de Programación", abordando desde algoritmos simples en Java hasta programación orientada a objetos y estructuras de control, preparando a los estudiantes de nivel terciario para comprender las complejidades del desarrollo informático.`
       }
     ]
   };
 };
 
 const cargarPropsEducacion = () => {
-  var paragraphs;
   return {
     title: "Educación",
     listaCards: [
       {
         titleCard: "Licenciado en Sistemas",
         dondeYCuandoCard: "Universidad de Palermo (2018-2020)",
-        textoCard: paragraphs[0]
+        textoCard: `Trabajo Final de Grado : "Bot Docente". Proyecto desarrollado en Python utilizando Telegram Bot API y librerías de IA, como SpaCy y NLTK. Despliegue de un bot conversacional disponible en la plataforma Telegram, como apoyo complementario a la labor docente.`
       },
       {
         titleCard: "Analista de Sistemas",
         dondeYCuandoCard: "Instituto Tecnológico ORT (2013-2017)",
-        textoCard: paragraphs[1]
+        textoCard: `Proyecto Final destacado: Desarrollo de una aplicación web PHP para un emprendimiento de Yoga y Pilates.
+        <br>
+        <br>
+        Tecnologías: Symfony Framework (PHP), MySQL, Amazon Web Services`
       },
       {
         titleCard: "Bachiller Técnico con orientación en Informática",
         dondeYCuandoCard: "Escuela ORT (2007-2012)",
-        textoCard: paragraphs[2]
+        textoCard: `Proyecto final Destacado: Desarrollo de una aplicación desktop para la creación de partituras musicales utilizando la tecnología de Microsoft Kinect, en el framework .NET .`
       }
     ]
   };
